bdc1fb9de9ef7bf18bfc81637f6e4f35f171c61b
Justin C. Beck <justinbeck@mac.com>
2018-12-23 15:03:06 -0500

    add some comments to document my theory

--- lib/compass/averager.rb
+++ lib/compass/averager.rb
@@ -8,10 +8,19 @@ module Compass
       rarg = [a, b].min
 
       if larg - rarg < 180
+        # if the delta is less than 180 then we can
+        # just add together and divide by 2
         average = (larg + rarg) / 2
       else
+        # if the delta is greater than 180 then we
+        # need to normalize the scale by adding 360
+        # to the smaller number
         rarg = rarg + 360
+
+        # then we can calculate the average as usual
         average = (larg + rarg) / 2
+
+        # then we need to reduce back below 360
         while (average > 360)
           average = average - 360
         end
