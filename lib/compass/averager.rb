00a529d9e32fad716a4be9645d36d4b3bd82ad68
Justin C. Beck <justinbeck@mac.com>
2018-12-25 11:21:34 -0500

    finish support for radians, add test cases for coverage

--- lib/compass/averager.rb
+++ lib/compass/averager.rb
@@ -3,19 +3,20 @@
 
 module Compass
   class Averager
+    
     def average(a, b, radians = false)
       larg = [a, b].max
       rarg = [a, b].min
 
-      # if !radians
-      #   lrad = convert_to_radians(larg)
-      #   rrad = convert_to_radians(rarg)
-      # else
+      if !radians
+        lrad = convert_to_radians(larg)
+        rrad = convert_to_radians(rarg)
+      else
         lrad = larg
         rrad = rarg
-      # end
+      end
 
-      if lrad - rrad < 180
+      if lrad - rrad < Math::PI
         # if the delta is less than 180 then we can
         # just add together and divide by 2
         average = (lrad + rrad) / 2
@@ -23,22 +24,27 @@ module Compass
         # if the delta is greater than 180 then we
         # need to normalize the scale by adding 360
         # to the smaller number
-        rrad = rrad + 360
+        rrad = rrad + (2 * Math::PI)
 
         # then we can calculate the average as usual
         average = (lrad + rrad) / 2
 
         # then we need to reduce back below 360
-        while (average > 360)
-          average = average - 360
+        while (average > (2 * Math::PI))
+          average = average - (2 * Math::PI)
         end
       end
 
-      puts "Average: #{average}"
+      average = radians ? average : convert_to_degrees(average)
+      average
     end
 
     def convert_to_radians(degrees)
-      (degrees / 360.0) * (2.0 * Math::PI)
+      (degrees / 180.0) * Math::PI
+    end
+
+    def convert_to_degrees(radians)
+      radians / Math::PI * 180.0
     end
   end
 end
--- spec/compass/averager_spec.rb
+++ spec/compass/averager_spec.rb
@@ -2,51 +2,99 @@ require 'spec_helper'
 
 module Compass
   RSpec.describe Averager do
-    context "#average" do
+    context "#average all the degrees" do
       let(:averager) { Averager.new }
 
-      it "average two values in quadrant 1" do
-        expect { averager.average(5, 85) }.to output("Average: 45\n").to_stdout
+      it "average two degree degree values in quadrant 1" do
+        expect((averager.average(5, 85)).round(1)).to eql(45.0)
       end
 
-      it "average two values in quadrant 2" do
-        expect { averager.average(95, 175) }.to output("Average: 135\n").to_stdout
+      it "average two degree values in quadrant 2" do
+        expect(averager.average(95, 175)).to eql(135.0)
       end
 
-      it "average two values in quadrant 3" do
-        expect { averager.average(185, 265) }.to output("Average: 225\n").to_stdout
+      it "average two degree values in quadrant 3" do
+        expect(averager.average(185, 265)).to eql(225.0)
       end
 
-      it "average two values in quadrant 4" do
-        expect { averager.average(275, 355) }.to output("Average: 315\n").to_stdout
+      it "average two degree values in quadrant 4" do
+        expect(averager.average(275, 355)).to eql(315.0)
       end
 
-      it "average two values in quadrants 1 & 2" do
-        expect { averager.average(45, 135) }.to output("Average: 90\n").to_stdout
+      it "average two degree values in quadrants 1 & 2" do
+        expect(averager.average(45, 135)).to eql(90.0)
       end
 
-      it "average two values in quadrants 2 & 3" do
-        expect { averager.average(135, 225) }.to output("Average: 180\n").to_stdout
+      it "average two degree values in quadrants 2 & 3" do
+        expect(averager.average(135, 225)).to eql(180.0)
       end
 
-      it "average two values in quadrants 3 & 4" do
-        expect { averager.average(225, 315) }.to output("Average: 270\n").to_stdout
+      it "average two degree values in quadrants 3 & 4" do
+        expect(averager.average(225, 315)).to eql(270.0)
       end
 
-      it "average two values in quadrants 1 & 3 (delta < 180)" do
-        expect { averager.average(50, 220) }.to output("Average: 135\n").to_stdout
+      it "average two degree values in quadrants 1 & 3 (delta < 180)" do
+        expect(averager.average(50, 220)).to eql(135.0)
       end
 
-      it "average two values in quadrants 2 & 4 (delta < 180)" do
-        expect { averager.average(140, 310) }.to output("Average: 225\n").to_stdout
+      it "average two degree values in quadrants 2 & 4 (delta < 180)" do
+        expect(averager.average(140, 310)).to eql(225.0)
       end
 
-      it "average two values in quadrants 1 & 3 (delta > 180)" do
-        expect { averager.average(40, 230) }.to output("Average: 315\n").to_stdout
+      it "average two degree values in quadrants 1 & 3 (delta > 180)" do
+        expect(averager.average(40, 230)).to eql(315.0)
       end
 
-      it "average two values in quadrants 2 & 4 (delta > 180)" do
-        expect { averager.average(130, 320) }.to output("Average: 45\n").to_stdout
+      it "average two degree values in quadrants 2 & 4 (delta > 180)" do
+        expect((averager.average(130, 320)).round(1)).to eql(45.0)
+      end
+    end
+
+    context "#average all the radians" do
+      let(:averager) { Averager.new }
+
+      it "average two degree degree values in quadrant 1" do
+        expect(averager.average(0.08726646259971647, 1.48352986419518, true)).to eql(0.7853981633974482)
+      end
+
+      it "average two degree values in quadrant 2" do
+        expect(averager.average(1.6580627893946132, 3.0543261909900767, true)).to eql(2.356194490192345)
+      end
+
+      it "average two degree values in quadrant 3" do
+        expect(averager.average(3.2288591161895095, 4.625122517784973, true)).to eql(3.9269908169872414)
+      end
+
+      it "average two degree values in quadrant 4" do
+        expect(averager.average(4.799655442984406, 6.19591884457987, true)).to eql(5.497787143782138)
+      end
+
+      it "average two degree values in quadrants 1 & 2" do
+        expect(averager.average(0.7853981633974483, 2.356194490192345, true)).to eql(1.5707963267948966)
+      end
+
+      it "average two degree values in quadrants 2 & 3" do
+        expect(averager.average(2.356194490192345, 3.9269908169872414, true)).to eql(3.141592653589793)
+      end
+
+      it "average two degree values in quadrants 3 & 4" do
+        expect(averager.average(3.9269908169872414, 5.497787143782138, true)).to eql(4.71238898038469)
+      end
+
+      it "average two degree values in quadrants 1 & 3 (delta < 180)" do
+        expect(averager.average(0.8726646259971648, 3.839724354387525, true)).to eql(2.356194490192345)
+      end
+
+      it "average two degree values in quadrants 2 & 4 (delta < 180)" do
+        expect(averager.average(2.443460952792061, 5.410520681182422, true)).to eql(3.9269908169872414)
+      end
+
+      it "average two degree values in quadrants 1 & 3 (delta > 180)" do
+        expect(averager.average(0.6981317007977318, 4.014257279586958, true)).to eql(5.497787143782138)
+      end
+
+      it "average two degree values in quadrants 2 & 4 (delta > 180)" do
+        expect(averager.average(2.2689280275926285, 5.585053606381854, true)).to eql(0.7853981633974492)
       end
     end
 
